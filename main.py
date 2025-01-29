2cbeaa7e50047ba1340a4c5f8e40d6e947747bbe
shoshanarosenthal <sr0527665479@gmail.com>
2025-01-29 12:46:30 +0200

    origin

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://github.com/shoshanarosenthal/front_render.git"],  # Allows all origins, change this to restrict specific origins if needed
+    allow_origins=["*"],  # Allows all origins, change this to restrict specific origins if needed
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
